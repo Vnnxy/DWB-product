98f324cd7892085785e4364a0d3b8474b4dddf88
Htska <112268934+Htska@users.noreply.github.com>
2025-02-25 21:05:56 -0600

    Practica 3

---
+++ src/main/java/com/product/api/controller/CtrlCategory.java
@@ -0,0 +1,27 @@
+package com.product.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/category")
+public class CtrlCategory {
+
+    @Autowired
+    SvcCategory svc;
+
+    @GetMapping()
+    public List<Category> getCategories() {
+        return svc.getCategories();
+    }
+    
+}
