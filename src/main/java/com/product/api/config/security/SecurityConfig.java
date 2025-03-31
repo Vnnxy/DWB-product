6a31e948e002f6a663490402fbd892b7e8e422fe
Miguel Akira López Asano <118841701+Vnnxy@users.noreply.github.com>
2025-03-30 19:33:39 -0600

    Added the Login P7

---
+++ src/main/java/com/product/api/config/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.product.api.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.product.api.config.jwt.JwtAuthFilter;
+
+/**
+ * Clase que define los permisos para cada endpoint
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfig corsConfig) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/error", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/info",
+                                        "/actuator/health")
+                                .permitAll()
+                                // category
+                                .requestMatchers(HttpMethod.GET, "/category/active")
+                                .hasAnyAuthority("ADMIN", "CUSTOMER")
+                                .requestMatchers("/category/**").hasAuthority("ADMIN")
+                                // product
+                                .requestMatchers(HttpMethod.GET, "/product/*").hasAnyAuthority("ADMIN", "CUSTOMER")
+                                .requestMatchers("/product/**").hasAuthority("ADMIN")
+                                // customer-images
+                                .requestMatchers("/product-image/**").hasAnyAuthority("ADMIN", "CUSTOMER")
+
+                )
+
+                .cors(cors -> cors.configurationSource(corsConfig))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.disable())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
